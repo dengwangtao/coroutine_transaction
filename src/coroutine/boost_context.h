ec9a6e4c7089d6e42edc939320f031726a1afd33
wangtaodeng <wangtaodeng@tencent.com>
2025-03-21 16:51:31 +0800

    feat: 实现多协程调度

--- src/coroutine/boost_context.h
+++ src/coroutine/boost_context.h
@@ -7,9 +7,25 @@
 using BoostContext = void*;
 using BoostCoroutineFunc = void (*)(void* param);
 
+/**
+ * @brief 跳转上下文
+ * @param ofc 原上下文
+ * @param nfc 目标上下文
+ * @param param 参数
+ * @param preserve_fpu 是否保留浮点寄存器
+ * @return 0 成功
+ */
 extern "C"
 s64 JumpContext(BoostContext* ofc, BoostContext nfc, void* param, bool preserve_fpu = false);
 
+
+
+/**
+ * @brief 创建上下文
+ * @param stack 栈地址
+ * @param size 栈大小
+ * @param func 函数
+ */
 extern "C"
 BoostContext MakeContext(void* stack, size_t size, BoostCoroutineFunc func);
 
