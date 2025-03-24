dffa43ce4d1b151d211263bd9829fb142b2895b4
wangtaodeng <wangtaodeng@tencent.com>
2025-03-24 11:22:59 +0800

    feat: 在 main 中新增 TestEnum 测试并更新 proto 定义

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,26 @@
 
 
 
-#include "test_coroutine.h"
+// #include "test_coroutine.h"
 
 // #include "test_other.h"
+
+
+#include "error_define.pb.h"
+int main()
+{
+    TestEnum test_enum;
+
+    u32 value1 = 1;
+    u32 value2 = 200;
+
+    auto t1 = static_cast<TestEnum>(value1);
+    auto t2 = static_cast<TestEnum>(value2);
+
+    std::cout << t1 << std::endl;
+    std::cout << t2 << std::endl;
+
+
+    return 0;
+
+}
