d20e166025681c0326191d85d0c736898e3cacaa
taotao <dengwangtao@126.com>
2025-03-27 17:54:22 +0800

    feat(dwt_muduo): 添加Acceptor、Buffer、EventLoop等模块并升级C++标准至14

--- demo/trans_server.cpp
+++ demo/trans_server.cpp
@@ -11,6 +11,30 @@ s32 DemoTransactionServer::Init()
         return ret;
     }
 
+    dwt::InetAddress addr("127.0.0.1", 8080);
+    event_loop_ = std::make_unique<dwt::EventLoop>();
+    if (event_loop_ == nullptr)
+    {
+        LogError() << "make event_loop failed";
+        return -1;
+    }
+
+    tcp_server_ = std::make_unique<dwt::TcpServer>(event_loop_.get(), addr, "DemoTransactionServer");
+    if (tcp_server_ == nullptr)
+    {
+        LogError() << "make tcp_server failed";
+        return -2;
+    }
+
+    tcp_server_->setConnectionCallback(
+        std::bind(&DemoTransactionServer::OnConnection, this, std::placeholders::_1)
+    );
+
+    tcp_server_->setMessageCallback(
+        std::bind(&DemoTransactionServer::OnMessage, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
+    );
+
+
     // g_trans_server_ptr 全局只有一个
     auto* addr_server = &DemoTransactionServer::Instance();
     g_trans_server_ptr = addr_server;
@@ -32,4 +56,29 @@ Transaction* DemoTransactionServer::GetTranByType(s32 type) const
     }
 
     return nullptr;
+}
+
+s32 DemoTransactionServer::Start(u32 thread_num)
+{
+    tcp_server_->setThreadNum(thread_num);
+    tcp_server_->start();
+    event_loop_->loop();
+    return 0;
+}
+
+void DemoTransactionServer::OnConnection(const dwt::TcpConnectionPtr& conn)
+{
+    LogInfo() << "new connection from " << conn->peerAddress().toIpPort();
+    if (! conn->connected())
+    {
+        LogInfo() << "connection from " << conn->peerAddress().toIpPort() << " closed";
+        conn->shutdown();
+    }
+}
+
+void DemoTransactionServer::OnMessage(const dwt::TcpConnectionPtr& conn, dwt::Buffer* buf, dwt::Timestamp time)
+{
+    std::string data = buf->retrieveAllAsString();
+    LogInfo() << "receive data from " << conn->peerAddress().toIpPort() << " data=" << data;
+    conn->send(data);
 }
--- demo/trans_server.h
+++ demo/trans_server.h
@@ -5,6 +5,7 @@
 
 #include "transaction_server.h"
 #include "trans_type.pb.h"
+#include "TcpServer.h"
 
 // =================== 事务服务器 ====================
 
@@ -12,9 +13,18 @@ class DemoTransactionServer : public TransactionServer, public Singleton<DemoTra
 {
 
 public:
+
     s32 Init() override;
 
     Transaction *GetTranByType(s32 type) const override;
+
+    s32 Start(u32 thread_num = 4);
+    void OnConnection(const dwt::TcpConnectionPtr& conn);
+    void OnMessage(const dwt::TcpConnectionPtr& conn, dwt::Buffer* buf, dwt::Timestamp time);
+
+private:
+    std::unique_ptr<dwt::TcpServer> tcp_server_;
+    std::unique_ptr<dwt::EventLoop> event_loop_;
 };
 
 
